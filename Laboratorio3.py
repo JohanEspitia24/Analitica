730e10fe97f0cf87b3746bf5b7ec4123b47ee059
JohanEspitia24 <johan.espitia.c@gmail.com>
2024-04-08 12:13:58 -0500

    Vamos encontrando las relaciones

--- Laboratorio3.py
+++ Laboratorio3.py
@@ -22,3 +22,20 @@ df2 = pd.DataFrame(iter(table2))
 print(df1.head())
 print(df2.head())
 
+# Mostrar las columnas
+print(df1.columns)
+print(df2.columns)
+
+#Usando mlxtend encontramos la relacion entre los productos vendidos
+#y la cantidad de veces que se vendieron juntos
+# Convert the DataFrame to a transaction list
+transactions = df1.values.tolist()
+
+# Apply one-hot encoding to the transaction list
+one_hot_encoded = frequent_patterns.transactionlist_to_dataframe(transactions, colnames=df1.columns)
+
+# Find frequent itemsets using the Apriori algorithm
+frequent_itemsets = frequent_patterns.apriori(one_hot_encoded, min_support=0.1, use_colnames=True)
+
+# Print the frequent itemsets
+print(frequent_itemsets)
